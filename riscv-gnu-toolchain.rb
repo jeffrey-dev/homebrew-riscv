80cff99a22f9e8006c8497a76419551e02e79f70
Jeffrey van Barneveld <jeffrey.barneveld@jeffdev.nl>
2025-03-07 16:13:22 +0100

    aa

--- riscv-gnu-toolchain.rb
+++ riscv-gnu-toolchain.rb
@@ -60,8 +60,7 @@ class RiscvGnuToolchain < Formula
 
     args = [
       "--prefix=#{prefix}",
-      "--with-cmodel=medany",
-      "--enable-default-pie"
+      "--with-cmodel=medany"
     ]
     args << "--enable-multilib" unless build.with?("NOmultilib")
 
